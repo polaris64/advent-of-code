a8f09c9ccfdc50f5c7b8c595c45e5e2dfb3569a5
Simon Pugnet <simon@polaris64.net>
2021-12-27 16:27:08 +0100

    Optimise 2021/15 (priority queue, etc.)

--- 2021/15/solve.py
+++ 2021/15/solve.py
@@ -1,33 +1,34 @@
+from queue import PriorityQueue
+
 def read_input(filename):
     with open(filename, "rt") as fh:
         return [[int(x) for x in line.strip()] for line in fh.readlines()]
 
 def gen_grid(inp, mul = (1, 1)):
+    len_x = len(inp[0])
+    len_y = len(inp)
     for y in range(len(inp) * mul[1]):
         row = []
-        y_norm = y % len(inp)
-        for x in range(len(inp[0]) * mul[0]):
-            if x < len(inp[0]) and y < len(inp):
+        y_norm = y % len_y
+        for x in range(len_x * mul[0]):
+            if x < len(inp[0]) and y < len_y:
                 num = inp[y][x]
             else:
-                src_num = inp[y_norm][x % len(inp[0])] - 1
+                src_num = inp[y_norm][x % len_x] - 1
                 num = src_num
-                num += (y // len(inp))
-                num += (x // len(inp[0]))
+                num += (y // len_y)
+                num += (x // len_x)
                 num = num % 9
                 num += 1
             row.append(num)
         yield row
 
 def get_neighbours(cell, inp, mul = (1, 1)):
-    neighbours = []
+    len_x = len(inp[0]) * mul[0]
+    len_y = len(inp) * mul[1]
     offsets = ((-1, 0), (0, -1), (1, 0), (0, 1))
-    for ox, oy in offsets:
-        nx = cell[0] + ox
-        ny = cell[1] + oy
-        if nx >= 0 and ny >= 0 and nx < len(inp[0]) * mul[0] and ny < len(inp) * mul[1]:
-            neighbours.append((nx, ny))
-    return neighbours
+    neighbours = [(cell[0] + ox, cell[1] + oy) for ox, oy in offsets]
+    return [n for n in neighbours if n[0] >= 0 and n[1] >= 0 and n[0] < len_x and n[1] < len_y]
 
 def reconstruct_path(came_from, current):
     total_path = [current]
@@ -38,30 +39,28 @@ def reconstruct_path(came_from, current):
     return total_path
 
 def astar(grid, start_pos, goal_pos):
-    open_set = set([start_pos])
+    open_queue = PriorityQueue()
+    open_queue.put((0, start_pos))
     came_from = dict()
 
-    def h(cell):
-        return abs(goal_pos[0] - cell[0]) + abs(goal_pos[1] - cell[1])
-
+    f_score = dict()
     g_score = dict()
     for y, row in enumerate(grid):
         for x, _ in enumerate(row):
             g_score[(x, y)] = 9999999
+            f_score[(x, y)] = abs(goal_pos[0] - x) + abs(goal_pos[1] - y)
     g_score[start_pos] = 0
 
-    while len(open_set) > 0:
-        current = min([(g_score[c] + h(c), c) for c in open_set])[1]
+    while open_queue.not_empty:
+        current = open_queue.get()[1]
         if current == goal_pos:
             return reconstruct_path(came_from, current)
-        open_set.remove(current)
         for n in get_neighbours(current, grid):
             tent_g_score = g_score[current] + grid[n[1]][n[0]]
             if tent_g_score < g_score[n]:
                 came_from[n] = current
                 g_score[n] = tent_g_score
-                if n not in open_set:
-                    open_set.add(n)
+                open_queue.put((g_score[n] + f_score[n], n))
 
 def render_grid(inp, path):
     res = ""
